aaea8177d6c1609843f46a863100995f3c14b7e6
zsiros kata <zsiros.kata02>
2024-10-15 11:04:20 +0200

    Add project files.

---
+++ nevesCucc1015/Program.cs
@@ -0,0 +1,38 @@
+﻿using nevesCucc1015.src;
+using System.Collections.Concurrent;
+using System.Text;
+
+List<Competitor> competitors = new();
+
+using StreamReader sr = new StreamReader(
+    path: @"..\..\..\src\forras.txt",
+    encoding: Encoding.UTF8
+    );
+while (!sr.EndOfStream)
+{
+    competitors.Add(new Competitor(sr.ReadLine()));
+}
+sr.Close();
+
+Console.WriteLine($"Célba érkezők száma: {competitors.Count}");
+
+//első célbaért női versenyző
+var f1 = competitors.Where(x=> !x.Gender).MinBy(x => x.TotalTime);
+Console.WriteLine($"első célbaért nő {f1}");
+
+//hány % férfi a versenyzőknek
+var f2 = competitors.Count(c=> c.Gender)/ (float)competitors.Count *100;
+Console.WriteLine($"A versenyzőknek {f2:0.00}% férfi");
+
+//átlagos depóban töltött idő
+var f3 = competitors.Average(x => (x.RaceTimes["1st depot"] + x.RaceTimes["2nd depot"]).TotalSeconds)/2;
+Console.WriteLine($"átlagosan depóban töltött idő: {f3:0.00}másodperc");
+
+//korkategóriánként a versenyzők száma
+var f4 = competitors.GroupBy(x => x.AgeCategory).OrderBy(x=> x.Key);
+
+Console.WriteLine("versenyzők száma kategóriák szerint");
+foreach (var f in f4)
+{
+    Console.WriteLine($"\t{f.Key,11} : {f.Count(),2}fő");
+}
---
+++ nevesCucc1015/src/Competitor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nevesCucc1015.src
+{
+    internal class Competitor
+    {
+        public string Name { get; set; }
+        public int YearOfBirth { get; set; }
+        public int RaceNumber { get; set; }
+        public bool Gender { get; set; }
+        public string AgeCategory { get; set; }
+        public Dictionary<string, TimeSpan> RaceTimes { get; set; }
+        public double TotalTime => RaceTimes.Values.Sum(x=> x.TotalSeconds);
+        public override string ToString()
+        {
+            return $"[{RaceNumber}] - {Name} {AgeCategory} {(Gender ? "férfi" : "nő")}";
+        }
+
+        public Competitor(string row)
+        {
+            var data = row.Split(';');
+            Name = data[0];
+            YearOfBirth = int.Parse(data[1]);
+            RaceNumber = int.Parse(data[2]);
+            Gender = data[3] == "f";
+            AgeCategory = data[4];
+            RaceTimes = new()
+            {
+                { "swimming", TimeSpan.Parse(data[5]) },
+                { "1st depot", TimeSpan.Parse(data[6])},
+                { "cycling", TimeSpan.Parse(data[7])},
+                { "2nd depot", TimeSpan.Parse(data[8])},
+                { "running", TimeSpan.Parse(data[9])},
+            };
+
+        }
+    }
+}
